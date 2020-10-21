b58e7c0101a854ad0ec0b697a03223ac7873be3f
Arique <60521756+Arique1104@users.noreply.github.com>
2020-10-20 19:50:27 -0500

    add a connection pkey reset to my ActiveRecord

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -9,17 +9,23 @@ class Api::V1::MerchantsController < ApplicationController
   end
 
   def create
-    new_merchant = Merchant.create!(merchant_params)
-    render json: MerchantSerializer.new(new_merchant)
+    ActiveRecord::Base.connection.reset_pk_sequence!('merchants')
+    Merchant.create!(merchant_params)
+    render json: MerchantSerializer.new(Merchant.last)
   end
 
   def destroy
-    merchant = Merchant.find(merchant_params[:id])
+    merchant = Merchant.find_by(destroy_params)
     merchant.destroy
+    render json: {}, status: :no_content
   end
 
 private
   def merchant_params
-    params.permit(:name, :id)
+    params.permit(:name)
+  end
+
+  def destroy_params
+    params.permit(:id)
   end
 end
--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -26,7 +26,18 @@ describe "Merchants API" do
     merchant = create_list(:merchant, 1)
     get "/api/v1/merchants/#{merchant.first.id}"
     expect(response).to be_successful
-    merchant = JSON.parse(response.body, symbolize_names: true)
+    merchant = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_an(String)
+
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(merchant[:attributes][:name]).to be_a(String)
+
+    expect(merchant[:attributes]).to have_key(:created_at)
+    expect(merchant[:attributes][:created_at]).to be_a(String)
+
+    expect(merchant[:attributes]).to have_key(:updated_at)
+    expect(merchant[:attributes][:updated_at]).to be_a(String)
   end
 
   it 'creates a merchant' do
@@ -35,7 +46,6 @@ describe "Merchants API" do
     post "/api/v1/merchants", :params => { name: "Addams Family Orchard"}
 #maybe useful for items?!
     # post "/api/v1/merchants", :params => {:merchant => { name: "Addams Family Orchard"}}
-
     json = JSON.parse(response.body, symbolize_names: true)
     new_merchant = json[:data]
     expect(new_merchant[:attributes][:name]).to eq(name)
@@ -47,11 +57,16 @@ describe "Merchants API" do
     new_merchant = Merchant.create!(name: "Lasondra's Art History Center: Austin Park")
 
     delete "/api/v1/merchants/#{new_merchant.id}", :params => {name: "#{new_merchant.name}"}
-    get '/api/v1/merchants'
-    expect(response).to be_successful
-    merchants = JSON.parse(response.body, symbolize_names: true)[:data]
 
-    expect(merchants.count).to eq(1)
+    expect(Merchant.find_by(:id => new_merchant.id)).to be_nil
+    #
+    # get '/api/v1/merchants'
+    # expect(response).to be_successful
+    # expect(response).to have_http_status(:created)
+    # merchants = JSON.parse(response.body, symbolize_names: true)[:data]
+    #
+    # require "pry"; binding.pry
+    # expect(merchants.count).to eq(1)
 
   end
 end
