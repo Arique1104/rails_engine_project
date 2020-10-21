3feea3179b74b59c93cdff96f670bef9127ee6a9
Arique <60521756+Arique1104@users.noreply.github.com>
2020-10-21 17:17:01 -0500

    Code that allows item index request to be successful

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -10,8 +10,7 @@ describe "Merchants API" do
 
     merchants.each do |merchant|
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(String)
-
+      expect(merchant[:id]).to be_a(String)
       expect(merchant[:attributes]).to have_key(:name)
       expect(merchant[:attributes][:name]).to be_a(String)
 
