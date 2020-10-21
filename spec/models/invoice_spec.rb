64dbe77fc62bc475bd2b6207a9222451b8c3b304
Arique <60521756+Arique1104@users.noreply.github.com>
2020-10-21 16:00:59 -0500

    Adds invoice items spec and model info, along with downloading all csv data

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe Invoice, type: :model do
   describe "relationships" do
     it { should belong_to :merchant }
     it { should belong_to :customer }
-    it { have_many :transactions }
+    it { should have_many :transactions }
+    it { should have_many :invoice_items }
+    it { should have_many(:items).through(:invoice_items)}
   end
 end
