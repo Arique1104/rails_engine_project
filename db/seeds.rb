ea1eeec527fe6cff162e991e7592884852a3579d
Arique <60521756+Arique1104@users.noreply.github.com>
2020-10-20 23:20:55 -0500

    Add customer csv data to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,13 @@ require 'CSV'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Merchant.destroy_all
+Customer.destroy_all
 
 CSV.foreach(Rails.root.join('db/data/merchants.csv'), headers: true) do |row|
   # row["name"] = row["name"].to_i
   Merchant.create(row.to_h)
+end
+
+CSV.foreach(Rails.root.join('db/data/customers.csv'), headers: true) do |row|
+  Customer.create(row.to_h)
 end
