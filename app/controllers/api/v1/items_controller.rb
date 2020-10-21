11ac34e2c0163889a8fcc45a9595c5f61a3ed331
Arique <60521756+Arique1104@users.noreply.github.com>
2020-10-21 17:36:08 -0500

    Adds show action to items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,5 +2,13 @@ class Api::V1::ItemsController < ApplicationController
   def index
     render json: ItemSerializer.new(Item.all)
   end
+  def show
+    render json: ItemSerializer.new(Item.find_by(item_params))
+  end
+
+  private
+  def item_params
+    params.permit(:id)
+  end
 
 end
--- spec/requests/api/v1/item_request_spec.rb
+++ spec/requests/api/v1/item_request_spec.rb
@@ -31,6 +31,31 @@ describe "Items API" do
         expect(item[:attributes]).to have_key(:merchant_id)
         expect(item[:attributes][:merchant_id]).to be_a(Integer)
       end
+    end
+
+    it "can get one item" do
+      merchant_1 = create_list(:merchant, 1)[0]
+      item = Item.create!(name: "Fries", description: "Deliciouso!", unit_price: 100.0, merchant_id: merchant_1.id )
+
+      get "/api/v1/items/#{item.id}"
+      expect(response).to be_successful
+
+      item = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(item).to have_key(:id)
+      expect(item[:id]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:name)
+      expect(item[:attributes][:name]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:description)
+      expect(item[:attributes][:description]).to be_a(String)
+
+      expect(item[:attributes]).to have_key(:unit_price)
+      expect(item[:attributes][:unit_price]).to be_a(Float)
+
+      expect(item[:attributes]).to have_key(:merchant_id)
+      expect(item[:attributes][:merchant_id]).to be_a(Integer)
 
   end
 end
