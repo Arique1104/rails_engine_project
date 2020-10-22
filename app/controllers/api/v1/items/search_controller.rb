e3c08c6ed97090bc21b72a2479d3f88bcf684f3b
Arique <60521756+Arique1104@users.noreply.github.com>
2020-10-22 17:25:35 -0500

    Relocates find method to items:search controller.

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -5,4 +5,20 @@ class Api::V1::Items::SearchController < ApplicationController
     render json: MerchantSerializer.new(merchant)
   end
 
+  def find
+    attribute = params.keys[0]
+    query = params[attribute]
+
+    if attribute == "created_at"
+      time = Date.parse(query)
+      @item = Item.where("#{attribute} > ?", "#{time.beginning_of_day}").limit(1)
+    elsif attribute == "updated_at"
+      time = Date.parse(query)
+      @item = Item.where("#{attribute} > ?", "#{time.beginning_of_day}").limit(1)
+    else
+      @item = Item.where("lower(#{attribute}) LIKE lower(?)", "%#{query}%").first
+    end
+    render json: ItemSerializer.new(@item)
+  end
+
 end
